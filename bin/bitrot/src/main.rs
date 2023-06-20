27d386d581e38c1b2e9cfcb355c110079624d956
Derek Frye <derek.frye@gmail.com>
2023-06-19 22:37:26 -0500

    bitrot: runs at least!

--- bin/bitrot/src/main.rs
+++ bin/bitrot/src/main.rs
@@ -23,33 +23,47 @@ fn main()-> io::Result<()>{
     // let data = fs::read_dir(mov)
     //     .expect("x");
 
-    let entriesd = fs::read_dir(mov)?
+    let movies = fs::read_dir(mov)?
+        .map(|res| res.map(|e| e.path()))
+        .collect::<Result<Vec<_>, io::Error>>()?;
+
+    let par2s = fs::read_dir(par2)?
         .map(|res| res.map(|e| e.path()))
         .collect::<Result<Vec<_>, io::Error>>()?;
 
     // let digest = md5::compute(guess);
     // println!("md5 {:x}", digest);
 
-    let xx = entriesd.iter();
+    let movies_iter = movies.iter();
+    let par2_iter = par2s.iter();
 
     let re = Regex::new(r"\.[Mm][4pP][vV4]$").unwrap();
     let md5ending =".md5.txt";
 
-    for x in xx {
+    for x in movies_iter {
         // https://stackoverflow.com/questions/37388107/how-to-convert-the-pathbuf-to-string
-        let mut zz = String::from(x.to_string_lossy());
+        let movie = x.to_string_lossy().into_owned();
         // println!("path {}", zz);
         
 
-        if re.is_match(&zz) {
+        
+        if re.is_match(&movie) {
 
-            let  tt = zz.push_str(md5ending).to_owned();
+            let mut par = String::from(par2);
+            let fdn = Path::new(&movie);
+            let filenm = String::from( fdn.file_name().unwrap().to_str().unwrap());
+            par.push_str(&filenm);
+            par.push_str(md5ending);
 
             // let abc = fs::metadata(tt).is_ok();
 
-            if (exists(&tt)) {
-
+            if fs::metadata(par.to_owned()).is_ok() {
+                println!("Exists: {}", par);
             }
+
+            // if (exists(zz)) {
+            //     println!("Exists: {}", zz);
+            // }
 
             // match (Path::new(&tt).exists()) {
             //     true => {
@@ -63,6 +77,6 @@ fn main()-> io::Result<()>{
     Ok(())
 }
 
-fn exists(zz: &str) -> bool {
+fn exists(zz: String) -> bool {
     fs::metadata(zz).is_ok()
 }
