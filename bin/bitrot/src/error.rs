5bc17566918a7781c70cbd141544f107e0f93e4a
Derek Frye <derek.frye@gmail.com>
2023-09-26 17:19:03 -0500

    bitrot: correctly append errors to a file

--- bin/bitrot/src/error.rs
+++ bin/bitrot/src/error.rs
@@ -3,14 +3,14 @@ use thiserror::Error;
 // taken from https://nick.groenen.me/posts/rust-error-handling/
 #[derive(Error, Debug)]
 pub enum AppError {
-    /// Represents an empty source. For example, an empty text file being given
-    /// as input to `count_words()`.
-    #[error("Missing MD5")]
-    EmptySource,
+    // /// Represents an empty source. For example, an empty text file being given
+    // /// as input to `count_words()`.
+    // #[error("Missing MD5")]
+    // EmptySource,
 
-    // /// Represents a failure to read from input.
-    // #[error("Mismatch MD5")]
-    // MismatchError,
+    // // /// Represents a failure to read from input.
+    // // #[error("Mismatch MD5")]
+    // // MismatchError,
     
     // ReadError { source: std::io::Error },
     /// Represents all other cases of `std::io::Error`.
--- bin/bitrot/src/main.rs
+++ bin/bitrot/src/main.rs
@@ -2,19 +2,22 @@ mod error;
 mod progress;
 // use crate::progress::hosting;
 
-use anyhow::{Context, Result};
+use anyhow::Result;
 use chrono::{Local, Timelike};
 use clap::Parser;
 use console::style;
 use md5::Digest;
 use regex::Regex;
 use std::fs;
+// use std::fs::File;
 use std::io;
 use std::io::BufRead;
+use std::io::Write;
 use std::path::{Path, PathBuf};
 use std::sync::mpsc::channel;
 use std::sync::mpsc::Sender;
 use std::time::Duration;
+use fs2::FileExt;
 
 // use crossbeam_channel::{unbounded, Sender};
 use std::thread;
@@ -29,8 +32,8 @@ fn main() -> Result<()> {
     }
 
     // let x = Path::new(&args.error_output_file);
-    let _=fs::write(&args.error_output_file, "tt\n");
-    
+    // let _=fs::write(&args.error_output_file, "tt\n");
+
     // }
 
     if args.mode == "ck" {
@@ -72,19 +75,19 @@ fn main() -> Result<()> {
         let zz = assign_work(data_files, args.thread_count);
         let (tx, rx) = channel();
 
-        let mut i = 0;
+        let mut status_bar_line_entry = 0;
         for x in zz {
             let tx1 = tx.clone();
             let kjdfj = args.path_to_cksums.clone();
             let jkdf = args.unit_testing.clone();
             thread::spawn(move || {
                 if jkdf {
-                    let _ = do_work(x.wrok, i, tx1);
+                    let _ = do_unit_tests(x.wrok, status_bar_line_entry, tx1);
                 } else {
-                    let _ = validate_ondisk_md5(x.wrok, &kjdfj, args.bufsize, i, tx1);
+                    let _ = validate_ondisk_md5(x.wrok, &kjdfj, args.bufsize, status_bar_line_entry, tx1);
                 }
             });
-            i += 1;
+            status_bar_line_entry += 1;
         }
         drop(tx);
 
@@ -95,22 +98,83 @@ fn main() -> Result<()> {
 
             let sb = xa[0].parse::<usize>().unwrap();
             let djk = xa[1];
+            // len of two just means we're starting to work on the file
             if xa.len() == 2 {
                 // pb[sb].set_message(format!("{djk}..."));
                 progress::set_message(sb, djk, &pb);
-            } else if xa.len() == 3 {
+            } 
+            // marking the file as done
+            else if xa.len() == 3 {
                 progress::finish_progress_bar(sb, &pb);
-            } else if xa.len() == 4 {
+            } 
+            // just incrementing progress for the file
+            else if xa.len() == 4 {
                 // pb[args.thread_count.to_string().parse::<usize>().unwrap()].set_message(format!("..."));
                 progress::increment_progress_bar(final_progress_bar, &pb);
-            } else if xa.len() == 7 {
+                // progress::finish_progress_bar(sb, &pb);
+            } 
+            // the par2 file didn't exist, wasn't readable, etc.
+            else if xa.len() == 5 {
+                let mut data = String::from("Error: ");
+                data.push_str(xa[1]);
+                // data.push_str(&format!(", expected: {}", xa[5]));
+                // data.push_str(&format!(", got: {}\n", xa[6]));
+                // data.p
+                // fs::write( &args.error_output_file, data).unwrap();
+
+                let mut fil = fs::OpenOptions::new()
+                .write(true)
+                .append(true)
+                .create(true)
+                .open(&args.error_output_file)?;
+                // let fil = File::open(&args.error_output_file)?;
+                fil.lock_exclusive()?;
+
+                // let mut xxx = fs::OpenOptions::new()
+                //     .write(true)
+                //     .append(true)
+                //     .create(true)
+                //     .open(&args.error_output_file)
+                //     .expect("cannot open file");
+                // let jkdfjkadf = data.clone();
+                fil.write(data.as_bytes()).unwrap();
+
+                fil.unlock()?;
+            }
+            // there was a checksum mismatch for the file
+            else if xa.len() == 7 {
                 let mut data = String::from("Error: ");
                 data.push_str(xa[4]);
                 data.push_str(&format!(", expected: {}", xa[5]));
                 data.push_str(&format!(", got: {}\n", xa[6]));
                 // data.p
-                fs::write( &args.error_output_file, data).unwrap();
-                }
+                // fs::write( &args.error_output_file, data).unwrap();
+
+                let mut fil = fs::OpenOptions::new()
+                .write(true)
+                .append(true)
+                .create(true)
+                .open(&args.error_output_file)?;
+                fil.lock_exclusive()?;
+
+                // let mut xxx = fs::OpenOptions::new()
+                //     .write(true)
+                //     .append(true)
+                //     .create(true)
+                //     .open(&args.error_output_file)
+                //     .expect("cannot open file");
+                // let jkdfjkadf = data.clone();
+                fil.write(data.as_bytes()).unwrap();
+                // write!( xxx, "{}", data.  );
+
+
+                fil.unlock()?;
+                //  let  jkdf= data
+
+                // let ipz = data.clone();
+
+                // zip(String::from(&data).as_str(), &args.error_output_file);
+            }
         }
 
         progress::finish_progress_bar(final_progress_bar, &pb);
@@ -122,7 +186,12 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-fn do_work(
+// fn zip (z: &str, zz: &str){
+//     let mut xxx = fs::OpenOptions::new().append(true).open(zz).expect("cannot open file");
+//     xxx.write(z.as_bytes());
+// }
+
+fn do_unit_tests(
     xx: Vec<PathBuf>,
     // path_to_cksums: &str,
     // bufsize: u16,
@@ -198,7 +267,7 @@ fn validate_ondisk_md5(
     par_path: &str,
     bufsize: u16,
     statusbar: u16,
-    ab: Sender<String>,
+    transmission_channel: Sender<String>,
 ) -> Result<(), anyhow::Error> {
     let md5ending = ".md5.txt";
 
@@ -207,11 +276,12 @@ fn validate_ondisk_md5(
         let movie_as_str = x.to_string_lossy();
         let movie_basename = x.file_name().unwrap().to_string_lossy();
 
+        // comes in as an auto-incremented integer, indicating which "line" on the pretty print output we can update
         let mut status_bar_and_working_file = statusbar.to_string();
         status_bar_and_working_file.push_str("|");
         status_bar_and_working_file.push_str(&movie_basename);
         // send we're working on file
-        ab.send(status_bar_and_working_file).unwrap();
+        transmission_channel.send(status_bar_and_working_file).unwrap();
 
         let mut par = String::from(par_path);
         par.push_str(&movie_basename); // /cksumpath/datafilenm
@@ -219,22 +289,45 @@ fn validate_ondisk_md5(
         let par_as_path = Path::new(&par);
 
         // if /par2path/movienm.md5.txt exists and is readable
-        if fs::metadata(par_as_path).is_ok() {
-            let digest = cksum(&movie_as_str, bufsize);
+        // if fs::metadata(par_as_path).is_ok() {
+        let digest = cksum(&movie_as_str, bufsize);
 
-            let mut md5hash_fromdisk = String::from("x");
+        // let mut md5hash_fromdisk = "x";
 
-            // got this ideas from initial question on https://stackoverflow.com/questions/75442962/how-to-do-partial-read-and-calculate-md5sum-of-a-large-file-in-rust
-            // reads just the first entries in teh file, before any spaces or newllines
-            if par_as_path.metadata().unwrap().len() > 0 {
-                md5hash_fromdisk = fs::read_to_string(par_as_path)
-                    .unwrap()
-                    .split_whitespace()
-                    .collect();
-            }
+        // got this ideas from initial question
+        // on https://stackoverflow.com/questions/75442962/how-to-do-partial-read-and-calculate-md5sum-of-a-large-file-in-rust
+        // reads just the first entries in teh file, before any spaces or newllines
+        // if par_as_path.metadata().unwrap().len() > 0 {
+        let zfdfas = fs::read_to_string(par_as_path).unwrap_or_else(|_| String::from("default"));
+
+        if "default" == zfdfas {
+            
+            
+            let mut status_bar_and_working_file = statusbar.to_string();
+            status_bar_and_working_file.push_str("|");
+            // pos 2
+            status_bar_and_working_file.push_str(format!("No md5 on disk found for {}\n", &movie_basename.trim()).as_str());
+            status_bar_and_working_file.push_str("|");
+            // pos 3
+            status_bar_and_working_file.push_str(" ");
+            status_bar_and_working_file.push_str("|");
+            // pos 4
+            status_bar_and_working_file.push_str(" ");
+            status_bar_and_working_file.push_str("|");
+            // pos 5
+            status_bar_and_working_file.push_str(&movie_basename);
+            transmission_channel.send(status_bar_and_working_file).unwrap();
+            
+            // i think (hope?) this return value is ignored
+            // return Err(error::AppError::EmptySource)
+            //     .context(format!("No md5 on disk found for {}", &movie_basename));
+        } else {
+            let md5hash_fromdisk = zfdfas.split_whitespace().next().unwrap();
+
+            let formatted_cksum = format!("{:x}", digest);
 
             // tell caller this integrity check failed
-            if md5hash_fromdisk != format!("{:x}", digest) {
+            if md5hash_fromdisk != formatted_cksum {
                 //Err(InvalidLookahead(movie_path));
                 // return Err(AppError::ConfigLoad { source: movie_path });
                 // return Err(error::AppError::MismatchError).context(format!(
@@ -264,12 +357,13 @@ fn validate_ondisk_md5(
                 status_bar_and_working_file.push_str(&format!("{:x}", digest));
 
                 // send msg
-                ab.send(status_bar_and_working_file).unwrap();
+                transmission_channel.send(status_bar_and_working_file).unwrap();
             }
-        } else {
-            return Err(error::AppError::EmptySource)
-                .context(format!("No md5 on disk found for {}", &movie_basename));
         }
+        // } else {
+        //     return Err(error::AppError::EmptySource)
+        //         .context(format!("No md5 on disk found for {}", &movie_basename));
+        // }
 
         // send we're done w/this unit of work
         let mut status_bar_and_working_file = statusbar.to_string();
@@ -280,7 +374,7 @@ fn validate_ondisk_md5(
         status_bar_and_working_file.push_str(" ");
         status_bar_and_working_file.push_str("|");
         status_bar_and_working_file.push_str(" ");
-        ab.send(status_bar_and_working_file).unwrap();
+        transmission_channel.send(status_bar_and_working_file).unwrap();
     }
     Ok(())
 }
@@ -329,7 +423,7 @@ struct Args {
     #[arg(short = 'm', long, value_name = "MODE")]
     mode: String,
 
-    /// Buffer size for reading files, in MiB. 512 seems to work well.
+    /// Buffer size for reading files, in MiB. 512 (default) seems to work well.
     #[arg(short, long, value_name = "BUFFERSIZE")]
     bufsize: u16,
 
@@ -337,7 +431,7 @@ struct Args {
     #[arg(short = 'r', long, value_name = "REGEX")]
     data_filename_match: String,
 
-    /// Number of threads to read and checksum data. I suggest set to num of cpu cores.
+    /// Number of threads to read and checksum data. I suggest set to num of cpu cores. Default 1.
     #[arg(short, long, value_name = "THREADCOUNT")]
     thread_count: u16,
 
